d008016c8247c95fde62b0d685014ebaf499a2d3
Anurag <anushab298@gmail.com>
2024-03-21 02:49:09 +0100

    EDA and Model Training

--- src/Exception.py
+++ src/Exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,ex_tb=error_detail.exc_info()
@@ -17,3 +17,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
+if __name__ == "__main__":
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info('Divided by Zero')
+        raise CustomException(e,sys)
